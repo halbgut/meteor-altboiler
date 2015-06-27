6b4e463169f90509d23429554561dc85f6e4f530
Kriegslustig <github@ls7.ch>
2015-06-27 20:11:15 +0200

    Implement the onLoad hook client side

--- assets/loader.js
+++ assets/loader.js
@@ -1,71 +1,93 @@
-// load gets called client side
-function load (includes) {
+// When this is used client side, altboiler wont be defined
+var loader = {}
+
+loader.load = function load (includes, onLoadQue) {
   var urlPrefix = jsCssPrefix = (__meteor_runtime_config__ && __meteor_runtime_config__.ROOT_URL_PATH_PREFIX) || '';
-  altboiler.loader.includeRenderer(document.head, includes, urlPrefix)
+  loader.onLoadQue.concat(onLoadQue)
+  loader.includeRenderer(document.head, includes, urlPrefix)
 }
 
-// When this is used client side, altboiler wont be defined
-var altboiler = altboiler || {}
+loader.onLoadQue = []
 
-altboiler.loader = {
-  includeRenderer: function includeRenderer (appendTo, includes, urlPrefix) {
-    var self = altboiler.loader
-    var allScripts = ''
-    var nthScriptLoaded = -1
+loader.onLoad = function onLoad (func) {
+  if(typeof func == 'string') func = eval('return ' + func)
+  if(func) loader.onLoadQue.push(func)
+}
 
-    function cleanUpAndAppend () {
-      document.body.removeChild(document.getElementById('boilerPlateLoader'))
-      appendTo.appendChild(self.createScriptTag(allScripts))
-    }
+loader.runOnLoadQue = function runOnLoadQue (que) {
+  que = que || loader.onLoadQue
+  que.shift()(loader.runOnLoadQue.bind(null, que))
+}
 
-    function loadScript (script) {
-      allScripts += '\n' + script
-    }
-
-    function loadedChecker (waitFor) {
-      return function () {
-        if(waitFor != nthScriptLoaded) return false
-        nthScriptLoaded++
-        return true
-      }
-    }
-
-    includes.forEach(function (include, index) {
-      self.fetchScript(
-        urlPrefix + include,
-        self.depChecker(loadedChecker(index - 1), loadScript)
-      )
-    })
-    self.depChecker(loadedChecker(includes.length - 1), cleanUpAndAppend)()
-  },
-
-  createScriptTag: function createScriptTag (script) {
-    var scriptElem = document.createElement('script')
-    scriptElem.type = 'text/javascript'
-    scriptElem.innerHTML = script
-    return scriptElem
-  },
-
-  fetchScript: function fetchScript (resource, callback) {
-    var self = altboiler.loader
-    var req = new XMLHttpRequest()
-    req.open('GET', resource)
-    req.addEventListener('load', self.ajaxResponseHandler(callback))
-    req.send()
-  },
-
-  ajaxResponseHandler: function ajaxResponseHandler (callback) {
-    return function () {
-      if(this.responseText) callback(this.responseText)
-    }
-  },
-
-  depChecker: function depChecker (isTrue, callback) {
-    var self = altboiler.loader
-    var args = arguments
-    return function (data) {
-      if(isTrue()) return callback(data)
-      setTimeout(self.depChecker.apply(null, args).bind(null, data), 100)
-    }
+loader.appender = function appender (head, body) {
+  return function appendBodyAndHead (next) {
+    if(head) document.head.innerHTML += head
+    if(body) document.body.innerHTML += body
+    next()
   }
 }
+
+loader.includeRenderer = function includeRenderer (appendTo, includes, urlPrefix) {
+  var allScripts = ''
+  var nthScriptLoaded = -1
+
+   loader.onLoad(function cleanUpAndAppend () {
+    document.body.removeChild(document.getElementById('boilerPlateLoader'))
+    appendTo.appendChild(loader.createScriptTag(allScripts))
+  })
+
+  function loadScript (script) {
+    allScripts += '\n' + script
+  }
+
+  function loadedChecker (waitFor) {
+    return function () {
+      if(waitFor != nthScriptLoaded) return false
+      nthScriptLoaded++
+      return true
+    }
+  }
+
+  includes.forEach(function (include, index) {
+    loader.fetchScript(
+      urlPrefix + include,
+      loader.depChecker(loadedChecker(index - 1), loadScript)
+    )
+  })
+  loader.depChecker(loadedChecker(includes.length - 1), loader.runOnLoadQue)()
+}
+
+loader.createScriptTag = function createScriptTag (script) {
+  var scriptElem = document.createElement('script')
+  scriptElem.type = 'text/javascript'
+  scriptElem.innerHTML = script
+  return scriptElem
+}
+
+loader.fetchScript = function fetchScript (resource, callback) {
+  var req = new XMLHttpRequest()
+  req.open('GET', resource)
+  req.addEventListener('load', loader.ajaxResponseHandler(callback))
+  req.send()
+}
+
+loader.ajaxResponseHandler = function ajaxResponseHandler (callback) {
+  return function () {
+    if(this.responseText) callback(this.responseText)
+  }
+}
+
+loader.depChecker = function depChecker (isTrue, callback) {
+  var args = arguments
+  return function (data) {
+    if(isTrue()) return callback(data)
+    setTimeout(loader.depChecker.apply(null, args).bind(null, data), 100)
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.altboiler = {}
+} else {
+  altboiler = altboiler || {}
+}
+altboiler.loader = loader
