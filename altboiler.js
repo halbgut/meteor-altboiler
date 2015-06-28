5c21ec61a9c99c2d738f9c78bc1b6310e1d29924
Kriegslustig <github@ls7.ch>
2015-06-28 22:33:33 +0200

    Add some comments

--- altboiler.js
+++ altboiler.js
@@ -1,9 +1,15 @@
-
-// Get some stuff closer
+/*
+ * This make my utils nicer to use
+ */
 var maniUtils = altboilerScope._maniUtils
 var templUtils = altboilerScope._templUtils
 var boilerUtils = altboilerScope._boilerUtils
 
+/*
+ * Gets all includes from the manifest
+ * It's used in different places inside this script,
+ * So its easiest to just put it here.
+ */
 var allIncludes = maniUtils.getIncludes(MANIFESTS[CURRENT_ARCH].manifest)
 
 /*
@@ -14,15 +20,31 @@ WebApp.connectHandlers.use(function (req, res, next) {
   res.end(altboiler.Boilerplate())
 })
 
+/*
+ * This serves the concateneted app script.
+ */
 WebApp.rawConnectHandlers.use(APP_SCRIPT, function (req, res, next) {
   res.end(maniUtils.getScripts(allIncludes['js']))
 })
 
+/* altboiler(options)
+ * `options` - an object containing options
+ * returns the new `altboiler.config`
+ * Adds the options to the exsisting `altboiler.config`.
+ */
 altboiler = function altboiler (options) {
-  altboiler.config = _.extend(options, altboiler.config)
+  return altboiler.config = _.extend(options, altboiler.config)
 }
 
+/*
+ * The default configuration
+ * Just sets `assets/default.html` as it's action
+ */
 altboiler.config = {
+  /* fadeOut(next)
+   * `next` - The callback
+   * A default onLoad hook that let's the `altboiler_boilerPlateLoader` disapear
+   */
   onLoad: [function fadeOut (next) {
     document.getElementById('altboiler_boilerPlateLoader').style.opacity = 0
     setTimeout(next, 200)
@@ -44,9 +66,12 @@ altboiler.onLoad = function onLoad (func) {
   return altboiler.config.onLoad.length - 1
 }
 
-
-// Renders a template and returns HTML
-// You can bind a context to it to use it as a context for the template
+/* altboiler.getTemplate(templateName)
+ * `templateName` - The name of a template
+ * returns the rendered template if its found, if not it returns `templateName`
+ * Renders a template and returns HTML
+ * You can bind a context to it to use it as a context for the template
+ */
 altboiler.getTemplate = function getTemplate (templateName) {
   var rawTemplate = templUtils.getRawTemplate(templateName)
   if(!rawTemplate) return templateName
@@ -54,6 +79,10 @@ altboiler.getTemplate = function getTemplate (templateName) {
   return SSR.render(templateName, this)
 }
 
+/* altboiler.Boilerplate()
+ * returns the rendered boilerplate
+ * It renderes the template `assets/main.html`
+ */
 // Returns the generated boilerplate
 altboiler.Boilerplate = function Boilerplate () {
   return altboiler.getTemplate.call(
