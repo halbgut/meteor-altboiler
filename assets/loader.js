5cdb7784007c7597dc9ad9266d60f30df7249465
Kriegslustig <github@ls7.ch>
2015-07-01 18:40:54 +0200

    Add some comments to assets/loader.js

--- assets/loader.js
+++ assets/loader.js
@@ -2,18 +2,34 @@ var loader = {}
 
 loader.onLoadQueue = []
 
+/* altboiler.loader.onLoad(func)
+ * `func` - A function or a function name or an array of these
+ * returns the index of the newly pushed functions
+ * pushes functions to the loader.onLoadQueue
+ */
 loader.onLoad = function onLoad (func) {
   if(typeof func == 'object') return func.forEach(this.onLoad)
   if(typeof func == 'string') eval('func = ' + func)
   if(func) return this.onLoadQueue.push(func) - 1
 }
 
+/* altboiler.loader.runOnLoadQueue([queue])
+ * `queue` - The remaining onLoadQue
+ * Recursively runs all functions inside loader.onLoadQueue in reverse order
+ */
 loader.runOnLoadQueue = function runOnLoadQueue (queue) {
   queue = queue || this.onLoadQueue
   if(!queue.length) return
   queue.pop()(this.runOnLoadQueue.bind(this, queue))
 }
 
+/* altboiler.loader.appender(head, body)
+ * `head` - code to be appended to the head element
+ * `body` - code to be appended to the body element
+ * returns a function that appends the stuff
+ * This is an internal helper.
+ * It's just to keep assets/main.html clean
+ */
 loader.appender = function appender (head, body) {
   return function appendBodyAndHead (next) {
     if(head) document.head.innerHTML += decodeURIComponent(head)
@@ -22,6 +38,12 @@ loader.appender = function appender (head, body) {
   }
 }
 
+/* altboiler.loader.removerBtId(elementId)
+ * `elementId` - The id property of the element to be removed
+ * returns a function that removes the element
+ * This is an internal helper.
+ * It's just to keep assets/main.html clean
+ */
 loader.removerById = function removerById (elementId) {
   return function (next) {
     var removeThis = document.getElementById(elementId)
