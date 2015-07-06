9dd0718713956d73655f18973b1ba4ae1ac69789
Kriegslustig <github@ls7.ch>
2015-07-06 21:48:38 +0200

    Test for a more testable internable API

--- tests/altboiler.js
+++ tests/altboiler.js
@@ -7,6 +7,23 @@ function cleanLB (str) {
   return str
 }
 
+function newReqStump () {
+  var end = ''
+  return {
+    0: {
+      originalUrl: ''
+    },
+    1: {
+      end: function (str) {
+        end = str
+      }
+    },
+    getEnd: function () {
+      return end
+    }
+  }
+}
+
 Tinytest.add('altboiler.config', function (test) {
   var altboiler = newAltboiler()
   test.equal(typeof altboiler.config, 'function', 'It should be a function')
@@ -17,24 +34,24 @@ Tinytest.add('altboiler.config', function (test) {
     onLoad: ['console.log("loadeed!")'],
     action: function () { return 'someAction...' },
   })
-  test.matches(cleanLB(altboiler.Boilerplate()), /<style\stype="text\/css">.*div\s{}/gm, 'The hooked css should be rendered')
-  test.matches(cleanLB(altboiler.Boilerplate()), /<script.*console\.log\("moaarr\sdrama!"\)/gm, 'The hooked js should be rendered')
-  test.matches(cleanLB(altboiler.Boilerplate()), /<script.*console\.log\("loadeed!"\)/gm, 'The hooked js should be rendered')
+  test.matches(cleanLB(altboiler.Boilerplate(altboiler.configuration)), /<style\stype="text\/css">.*div\s{}/gm, 'The hooked css should be rendered')
+  test.matches(cleanLB(altboiler.Boilerplate(altboiler.configuration)), /<script.*console\.log\("moaarr\sdrama!"\)/gm, 'The hooked js should be rendered')
+  test.matches(cleanLB(altboiler.Boilerplate(altboiler.configuration)), /<script.*console\.log\("loadeed!"\)/gm, 'The hooked js should be rendered')
   ; (function () {
     var altboiler = newAltboiler()
     altboiler.config({
       action: '<script>console.log("damn trolls")</script>'
     })
-    test.matches(cleanLB(altboiler.Boilerplate()), /<body>.*"damn\strolls"/gm, 'The action should be rendered inside the body')
+    test.matches(cleanLB(altboiler.Boilerplate(altboiler.configuration)), /<body>.*"damn\strolls"/gm, 'The action should be rendered inside the body')
   })()
 })
 
 Tinytest.add('altboiler.Boilerplate', function (test) {
   var altboiler = newAltboiler()
-  test.isTrue(altboiler.Boilerplate().indexOf('<body>') > -1, 'Its return value should contain a body element')
-  test.isFalse(altboiler.Boilerplate().indexOf('<DOCTYPE') > -1, 'Its return value should\'nt contain a doctype')
-  test.isTrue(altboiler.Boilerplate().indexOf('<head>') > -1, 'Its return value should contain a head element')
-  test.isTrue(altboiler.Boilerplate().indexOf('src="/altboiler/main.js"') > -1, 'It should render a script tag that gets the /altboiler/main.js')
+  test.isTrue(altboiler.Boilerplate(altboiler.configuration).indexOf('<body>') > -1, 'Its return value should contain a body element')
+  test.isFalse(altboiler.Boilerplate(altboiler.configuration).indexOf('<DOCTYPE') > -1, 'Its return value should\'nt contain a doctype')
+  test.isTrue(altboiler.Boilerplate(altboiler.configuration).indexOf('<head>') > -1, 'Its return value should contain a head element')
+  test.isTrue(altboiler.Boilerplate(altboiler.configuration).indexOf('src="/altboiler/main.js"') > -1, 'It should render a script tag that gets the /altboiler/main.js')
 })
 
 Tinytest.add('altboiler.renderAction', function (test) {
@@ -48,6 +65,10 @@ Tinytest.add('altboiler.set', function (test) {
   test.equal(typeof altboiler.set, 'function', 'It should be a function')
   test.isTrue(altboiler.set({test:true}).test, 'It should return the object that is passed to it.')
   altboiler.set({css: '.titanic {float: none}'})
-  test.isTrue(/\.titanic/g.test(altboiler.Boilerplate()), 'altboiler.Boilerplate should use the passed options')
-  test.isFalse(/\.titanic/g.test(altboiler.Boilerplate()), 'altboiler.Boilerplate should only use the passed options once')
+  var reqStump = newReqStump()
+  altboiler.serveBoilerplate(reqStump[0], reqStump[1])
+  test.isTrue(/\.titanic/g.test(reqStump.getEnd()), 'altboiler.Boilerplate should use the passed options')
+  reqStump = newReqStump()
+  altboiler.Boilerplate(reqStump[0], reqStump[1])
+  test.isFalse(/\.titanic/g.test(reqStump.getEnd()), 'altboiler.Boilerplate should only use the passed options once')
 })
