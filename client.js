182db5b480acc9a22f0d4e632d26bcb12ff8a4a4
Kriegslustig <github@ls7.ch>
2015-07-18 20:31:09 +0200

    Fix of a fix of client-side showLoader check

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 var configUtils = _altboilerScope.configUtils
 
-/* Wait some */
-addEventListener('onload', function () {
+/* Wait until the client-side configuration has been done */
+addEventListener('load', function () {
   /* Don't do anything if the loader should be shown */
   if(configUtils.isTruthy(altboiler.getConfig('showLoader'))) return
 
   /* Load the static CSS defined for the altboiler */
   document.head.innerHTML +=
-    ['<link rel="stylesheet" type="text/css" src="', '">']
+    ['<link rel="stylesheet" type="text/css" href="', '">']
       .join(altboiler.getConfig('routes').css)
 })
