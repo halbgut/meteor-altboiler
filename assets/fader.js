0b18681557971ac29f2092bd6242d0ea57a52d97
Kriegslustig <github@ls7.ch>
2015-07-29 22:11:39 +0200

    Fix the fader

--- assets/fader.js
+++ assets/fader.js
@@ -5,24 +5,41 @@
  * Otherwise the loadscreen would fade in and vanish
  * If the load time was under 400ms
  */
-var totalTimeStep = 400
-var timeSingleStep = 10
-var totalSteps = Math.round(totalTimeStep / timeSingleStep)
-var opacityStep = Math.round(100 / totalSteps) / 100
-window.altboiler_stopStepping = false
-function fadeIn () {
-  var targetElem = document.getElementById('altboiler_boilerPlateLoader')
-  fadeInALittle(targetElem, 0)
+
+var Fader = {
+  frameRate: 100,
+  time: 1000,
+  direction: 1,
+  opacity: 0,
+
+  calculateStep: function () {
+    this.step = 1 / this.time * this.frameRate
+  },
+
+  fade: function () {
+    var newOpacity = this.opacity + this.step * this.direction
+    if(this.stop || newOpacity < 0 || newOpacity > 1) return
+    this.target.style.opacity = this.opacity = newOpacity
+    setTimeout(this.fade.bind(this), this.frameRate)
+  }
 }
-function fadeInALittle (targetElem, oldOpacity) {
-  if(window.altboiler_stopStepping || oldOpacity > 1) return
-  newOpacity = targetElem.style.opacity = oldOpacity + opacityStep
-  setTimeout(fadeInALittle.bind(null, targetElem, newOpacity), timeSingleStep)
-}
-setTimeout(fadeIn, 0)
+
+var inFader = Object.create(Fader)
+inFader.target = document.getElementById('altboiler_boilerPlateLoader')
+inFader.calculateStep()
+inFader.fade()
 
 altboiler.loader.onLoad(function stopFadeIn (next) {
-  window.altboiler_stopStepping = true
-  document.getElementById('altboiler_boilerPlateLoader').style.opacity = 0
-  next()
+  var outFader = Object.create(Fader)
+  inFader.stop = true
+  outFader.opacity = inFader.opacity
+  outFader.target = inFader.target
+  outFader.direction = -1
+  outFader.calculateStep()
+  outFader.fade()
+
+  setTimeout(
+    next,
+    (outFader.time / outFader.opacity)
+  )
 })
