5c4981122946fbbe8c8bcece6ebc959dd942aa76
Kriegslustig <github@ls7.ch>
2015-08-02 16:55:26 +0200

    Fix the path to the linked loader css

--- client.js
+++ client.js
@@ -3,13 +3,16 @@ var configUtils = _altboilerScope.configUtils
 /* Wait until the client-side configuration has been done */
 addEventListener('load', function () {
   var showLoader = altboiler.getConfig('showLoader')
+  var styles
   /* Clear the object */
   altboiler.tmpConf = {}
   /* Don't do anything if the loader should be shown */
   if(configUtils.isTruthy(showLoader)) return
 
   /* Load the static CSS defined for the altboiler */
-  document.head.innerHTML +=
-    ['<link rel="stylesheet" type="text/css" href="', '">']
-      .join(altboiler.getConfig('routes').css)
+  styles = document.createElement('link')
+  styles.rel = 'stylesheet'
+  styles.type = 'text/css'
+  styles.href = altboiler.getConfig('routes').styles
+  document.head.appendChild(styles)
 })
