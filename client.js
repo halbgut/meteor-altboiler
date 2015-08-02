7eb6d175f65d70f0295d2deaac61660fc2c40def
Kriegslustig <github@ls7.ch>
2015-08-02 16:26:30 +0200

    Fix the client-side tmpConf

--- client.js
+++ client.js
@@ -2,8 +2,11 @@ var configUtils = _altboilerScope.configUtils
 
 /* Wait until the client-side configuration has been done */
 addEventListener('load', function () {
+  var showLoader = altboiler.getConfig('showLoader')
+  /* Clear the object */
+  altboiler.tmpConf = {}
   /* Don't do anything if the loader should be shown */
-  if(configUtils.isTruthy(altboiler.getConfig('showLoader'))) return
+  if(configUtils.isTruthy(showLoader)) return
 
   /* Load the static CSS defined for the altboiler */
   document.head.innerHTML +=
