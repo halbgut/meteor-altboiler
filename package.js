ec6d53f5449d72c704d52bacf7b5c57058b68d50
Kriegslustig <github@ls7.ch>
2016-02-27 11:50:26 +0100

    Update the package to 1.2.1

--- package.js
+++ package.js
@@ -7,13 +7,16 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3')
+  api.versionsFrom('1.2.1')
   api.use([
     'underscore',
     'webapp',
     'base64',
     'minifiers',
-    'spacebars-compiler'
+    'spacebars',
+    'spacebars-compiler',
+    'ejson',
+    'blaze'
   ])
 
   /* Importing all libs */
